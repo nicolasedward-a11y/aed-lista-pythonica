af4e2f7ff840da98f95df2ac46d219f86a67d2a3
nicolasedward-a11y <nicolas.edward@upe.br>
2026-04-16 18:36:18 -0300

    Update exercicio_10.py

--- src/exercicio_10.py
+++ src/exercicio_10.py
@@ -8,4 +8,4 @@ def greet_names(names: list[str]) -> list[str]:
     Returns:
         list[str]: lista com mensagens "Hello, <name>!"
     """
-    pass
+    return [f"Hello, {name}!" for name in names]
